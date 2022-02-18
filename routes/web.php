8fec55ed31620701393528a00e7547b3780f6678
monirpust <mdmonir1997@gmail.com>
2022-02-18 15:19:23 +0600

    blog page design added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +15,7 @@ use App\Http\Controllers\PagesController;
 */
 
 Route::get('/', [PagesController::Class, 'index'])->name('home');
+Route::get('/blog', [PostsController::Class, 'index'])->name('blog');
 Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
