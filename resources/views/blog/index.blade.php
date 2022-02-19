e2dbad86bf88607b553f3c06339785efcb00b112
monirpust <mdmonir1997@gmail.com>
2022-02-19 20:52:08 +0600

    dynamic post section added

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,26 +9,28 @@
     </div>
 </div>
 
-<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-    <div>
-        <img src="https://cdn.pixabay.com/photo/2015/03/30/14/07/coding-699318_960_720.jpg" width="700" alt="">
-    </div>
-    <div>
-        <h2 class="text-gray-700 font-bold text-5xl pb-4">
-            Learn how to do Laravel project
-        </h2>
-        <span class="text-gray-500">
-            By<span class="font-bold italic text-gray-800"> Learn with devMonir</span>
-            , 1 day ago
-        </span>
+@foreach ($posts as $post)
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2015/03/30/14/07/coding-699318_960_720.jpg" width="700" alt="">
+        </div>
+        <div>
+            <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                {{$post->title}}
+            </h2>
+            <span class="text-gray-500">
+                By<span class="font-bold italic text-gray-800"> {{$post->user->name}}</span>
+                , Created on {{date('jS M Y', strtotime($post->updated_at))}}
+            </span>
 
-        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae vel asperiores dolorem tempora rerum minima nulla fuga omnis ratione accusantium! Natus facere consequatur, libero reprehenderit provident minus aspernatur cumque corporis!
-        </p>
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+               {{$post->description}}
+            </p>
 
-        <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-            Keep Reading
-        </a>
+            <a href="/blog/{{$post->slut}}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                Keep Reading
+            </a>
+        </div>
     </div>
-</div>
+@endforeach
 @endsection
